3fea13d89443b2042790aefc32077c7df1b13f5f
TomasLadislaoDziurdz <tomidziurdzia@gmail.com>
2024-09-19 19:14:13 +0200

    Develop UpdateProduct PUT Endpoint with Minimal Apis and Carter

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -1,7 +1,4 @@
-﻿
-using Catalog.API.Products.GetProducts;
-
-namespace Catalog.API.Products.UpdateProduct
+﻿namespace Catalog.API.Products.UpdateProduct
 {
     public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         : ICommand<UpdateProductResult>;
